7b1e18a35218a19228726e808cf53d01b293d208
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2024-09-07 03:04:33 -0300

    Cleanup

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Text } from "react-native";
+import { Text } from "react-native"
 
 export default function MainPage() {
-  return <Text>Hello World!</Text>;
+  return <Text>Hello World!</Text>
 }
