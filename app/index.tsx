d8d4cb75bb7adc2e22f691c60ff2457b84149e1b
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2024-09-07 03:34:26 -0300

    Basic layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,21 @@
-import { Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 export default function MainPage() {
-  return <Text>Hello World!</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1}>Novos e recomendados</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 32,
+    paddingHorizontal: 16,
+  },
+  h1: {
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+})
