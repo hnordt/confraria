a46d62826863a1bb6f07b602ab52ac8b99afefed
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2024-09-15 15:09:15 -0300

    Fix

--- app/index.tsx
+++ app/index.tsx
@@ -49,7 +49,7 @@ const videos = [
   },
   {
     imageUrl:
-      "https://plus.unsplash.com/premium_photo-1678304224523-d25b4117558f?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Treino Express",
     category: 2,
     number: 4,
